e9987314e758fa7f891930268ab0ca383e3f647b
Tanaka925 <tawashi259@gmail.com>
2024-04-09 15:38:33 +0900

    アクションとルーティングを設定

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -1,4 +1,24 @@
 class ArtsController < ApplicationController
   def index
   end
+
+  def new
+    @art = Art.new
+  end
+
+  def create
+    @art = Art.new(art_params)
+    if @art.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def art_params
+    params.require(:art).permit(:image, :art_name, :artist_id, :work_id, :story)
+                        .merge(user_id: current_user.id)
+  end
 end
