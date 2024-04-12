259cbdff85d50fb1357c7c58f2fc763956d90a86
Tanaka925 <tawashi259@gmail.com>
2024-04-12 14:12:50 +0900

    作品編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'arts#index'
-  resources :arts, only: [:index, :new, :create, :show]
+  resources :arts, only: [:index, :new, :create, :show, :edit, :update]
 end
