eee9af32fbe76e323cd337d1fe627602b421b385
Tanaka925 <tawashi259@gmail.com>
2024-04-15 11:47:34 +0900

    バリデーション・アソシエーションの記述

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,18 @@
 class Artist < ApplicationRecord
+  validates :artist_name,         presence: true
+  validates :birthday,            presence: true
+
+  belongs_to :user
+
+  # アクティブハッシュ
+  self.data = [
+    { 'id': 1, 'name': '---' },
+    { 'id': 2, 'name': 'いちか' },
+    { 'id': 3, 'name': 'にこ' },
+    { 'id': 4, 'name': 'さんたろう' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :art
+  # /アクティブハッシュ
 end
